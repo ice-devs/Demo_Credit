2164f0179902d5d9b2c75e7f03940ff814fcad52
Otubu Othuke <othukeotubu@gmail.com>
2023-03-31 16:59:33 +0100

    refactor:refactored for prod

--- src/knex.ts
+++ src/knex.ts
@@ -2,19 +2,6 @@ import { Knex, knex } from 'knex'
 
 import {config} from './config/config';
 
-
-
-// const configs: Knex.Config = {
-//     client: config.dev.database,
-//     connection: {
-//         host : 'adminice.cqdgmiiqkcft.us-east-1.rds.amazonaws.com',
-//         port : 3306,
-//         user : 'adminice',
-//         password : 'adminice',
-//         database : 'adminice'
-//     },
-// };
-
 const configs: Knex.Config = {
     client: config.dev.database,
     connection: {
--- src/server.ts
+++ src/server.ts
@@ -15,7 +15,7 @@ import * as MODELS from './controllers/v0/index.model';
   console.log(await MODELS.createTables())
   
   const app = express();
-  const port = process.env.PORT || 8100;
+  const port = process.env.PORT || 8080;
 
   app.use(bodyParser.json());
 
